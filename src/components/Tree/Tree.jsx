08f9de887d430a18031d28f390590f4ebf1a1baf
Bartosz Nowacki <nowackib@protonmail.com>
2022-10-04 22:12:05 +0200

    tree do poprawki

--- src/components/Tree/Tree.jsx
+++ src/components/Tree/Tree.jsx
@@ -1,12 +1,49 @@
 import React from "react";
 
-const Tree = ({ data, key }) => {
-  console.log(data);
+/*const data = [
+  {
+    file: "documents",
+    subFiles: [
+      {
+        file: "27-10-1990",
+      },
+      {
+        file: "invoices",
+        subFiles: [
+          {
+            file: "electricityBills",
+            subFiles: [{ file: "invoice1" }, { file: "invoice2" }],
+          },
+          { file: "invoice12" },
+        ],
+      },
+    ],
+  },
+  {
+    file: "photos",
+    subFiles: [
+      {
+        file: "summer2020",
+        subFiles: [{ file: "10.25" }, { file: "10.26" }, { file: "10.27" }],
+      },
+    ],
+  },
+];*/
+
+const Tree = ({ data }) => {
+  console.log("File tree", data);
   return (
-    <div className="tree">
-      {data.map((el) => (
-        <p key={key}>{el.file}</p>
-      ))}
+    <div style={{ border: "solid 1px black" }}>
+      {data.map((el) => {
+        if ("subFiles" in el) {
+          console.log("subfiles", el);
+          return el.subFiles((x) => {
+            return <Tree data={x} />;
+          });
+        } else {
+          return <p>{el.file}</p>;
+        }
+      })}
     </div>
   );
 };
