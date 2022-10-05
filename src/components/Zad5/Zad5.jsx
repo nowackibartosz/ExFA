5483b90bece8aadc17f9abeda5d74f1cea623906
Bartosz Nowacki <nowackib@protonmail.com>
2022-10-05 14:24:45 +0200

    .

---
+++ src/components/Zad5/Zad5.jsx
@@ -0,0 +1,7 @@
+// ## Zadanie 5
+
+// Razem z Adrianem z JsMastery zaimplementuj stronę GPT-3
+// https://www.youtube.com/watch?v=LMagNcngvcU
+// będzie tam jeden hook jako zapowiedź przyszłej lekcji ;)
+
+// [Zadania dodatkowe]
